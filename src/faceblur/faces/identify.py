6c97645219b8fa55860ac92e9e42448813e79d06
Simona Dimitrova <nimeria1308@gmail.com>
2025-01-20 21:13:53 +0200

    Merge faces from close and far models

--- src/faceblur/faces/identify.py
+++ src/faceblur/faces/identify.py
@@ -1,15 +1,14 @@
 # Copyright (C) 2025, Simona Dimitrova
 
 import math
-import mediapipe as mp
 import numpy as np
 import os
 import tqdm
 
 from faceblur.av.container import InputContainer
+from mediapipe.python.solutions.face_detection import FaceDetection
 from PIL.Image import Image
 
-mp_face_detection = mp.solutions.face_detection
 
 IDENTIFY_IMAGE_SIZE = 1920
 
@@ -19,18 +18,48 @@ def _find_divisor(width, height, max_side):
     return math.ceil(side / max_side)
 
 
-def _identify_faces_from_image(image: Image, face_detection, image_size=IDENTIFY_IMAGE_SIZE):
-    # Cache original dimension as results are normalised
-    width = image.width
-    height = image.height
-
-    divisor = _find_divisor(image.width, image.height, image_size)
-    if divisor > 1:
-        # Needs to be scaled down
-        image = image.resize((image.width // divisor, image.height // divisor))
-
+def _merge_faces(faces_close, faces_far, confidence):
     faces = []
-    results = face_detection.process(np.array(image))
+
+    while faces_close:
+        faces_with_best_score = -1, -1
+        best_score = 0
+
+        # Find the best matches for each face
+        for face_close_index, face_close in enumerate(faces_close):
+            for face_far_index, face_far in enumerate(faces_far):
+                score = face_close.intersection_over_union(face_far)
+                if score > best_score:
+                    faces_with_best_score = face_close_index, face_far_index
+                    best_score = score
+
+        if best_score >= confidence:
+            # Found faces with enough similarity
+            face_close_index, face_far_index = faces_with_best_score
+            face_close = faces_close[face_close_index]
+            face_far = faces_far[face_far_index]
+
+            # Remove pair from further processing
+            faces_close.remove(face_close)
+            faces_far.remove(face_far)
+
+            # And add the union of both into the faces found so far
+            faces.append(face_close.union(face_far))
+        else:
+            # Could not find close faces that intersect with enough area
+            # Therefore all left faces are distinct
+            faces.extend(faces_close)
+            break
+
+    # If there are any left far faces, it means they must be distinct
+    faces.extend(faces_far)
+
+    return faces
+
+
+def _identify_faces_from_image_array(image, width, height, face_detection):
+    faces = []
+    results = face_detection.process(image)
     if results.detections:
         max_box = Box(0, width - 1, height - 1, 0)
 
@@ -48,6 +77,35 @@ def _identify_faces_from_image(image: Image, face_detection, image_size=IDENTIFY
             faces.append(face)
 
     return faces
+
+
+def _identify_faces_from_image(image: Image,
+                               face_detection_close,
+                               face_detection_far,
+                               merge_confidence,
+                               image_size):
+
+    # Cache original dimension as results are normalised
+    width = image.width
+    height = image.height
+
+    divisor = _find_divisor(image.width, image.height, image_size)
+    if divisor > 1:
+        # Needs to be scaled down
+        image = image.resize((image.width // divisor, image.height // divisor))
+
+    image = np.array(image)
+    faces_close = _identify_faces_from_image_array(image, width, height, face_detection_close)
+    faces_far = _identify_faces_from_image_array(image, width, height, face_detection_far)
+
+    # If either model did not find faces, simply return whatever the other one found (if any)
+    if not faces_close:
+        return faces_far
+    elif not faces_far:
+        return faces_close
+
+    # This means that both found faces and we need to group then and unite them into bigger boxes
+    return _merge_faces(faces_close, faces_far, merge_confidence)
 
 
 class Box:
@@ -80,11 +138,19 @@ class Box:
         intersection_left = max(self.left, other.left)
 
         # Check if there is an intersection
-        if intersection_bottom >= intersection_top and intersection_left <= intersection_right:
+        if intersection_top <= intersection_bottom and intersection_left <= intersection_right:
             return Box(intersection_top, intersection_right, intersection_bottom, intersection_left)
         else:
             # No intersection
             return None
+
+    def union(self, other):
+        # Calculate the union coordinates
+        union_top = min(self.top, other.top)
+        union_right = max(self.right, other.right)
+        union_bottom = max(self.bottom, other.bottom)
+        union_left = min(self.left, other.left)
+        return Box(union_top, union_right, union_bottom, union_left)
 
     def area(self):
         return (self.bottom - self.top + 1) * (self.right - self.left + 1)
@@ -95,20 +161,19 @@ class Box:
     def __eq__(self, other):
         return self.top == other.top and self.right == other.right and self.bottom == other.bottom and self.left == other.left
 
+    def intersection_over_union(self, other):
+        intersection = self.intersect(other)
+        if not intersection:
+            # Do not intersect
+            return 0
 
-def _intersection_over_union(box1, box2):
-    intersection = box1.intersect(box2)
-    if not intersection:
-        # Do not intersect
-        return 0
+        intersection_area = intersection.area()
 
-    intersection_area = intersection.area()
+        # area of the union
+        union_area = self.area() + other.area() - intersection_area
 
-    # area of the union
-    union_area = box1.area() + box2.area() - intersection_area
-
-    # intersection over union
-    return intersection_area / union_area
+        # intersection over union
+        return intersection_area / union_area
 
 
 def _track_faces(frames, min_score=0.5):
@@ -123,7 +188,7 @@ def _track_faces(frames, min_score=0.5):
             # Check if this face matches a track
             for track_index, track in enumerate(tracks):
                 # Compare against the most recent instance of the track
-                score = _intersection_over_union(face, track[-1])
+                score = face.intersection_over_union(track[-1])
                 if score > best_track_score:
                     best_track_score = score
                     best_track_index = track_index
@@ -152,7 +217,7 @@ def _interpolate_boxes(box1, box2, t):
     )
 
 
-def _interpolate_faces(frames, maximum_frame_depth=30, tracking_confidence=0.05):
+def _interpolate_faces(frames, tracking_frame_distance, tracking_confidence):
     tracks = _track_faces(frames, tracking_confidence)
 
     previous_faces = [
@@ -174,7 +239,7 @@ def _interpolate_faces(frames, maximum_frame_depth=30, tracking_confidence=0.05)
             # When was it last shown?
             previous_frame, previous_face = previous_faces[track_index]
             frame_distance = frame - previous_frame
-            if 1 < frame_distance < maximum_frame_depth:
+            if 1 < frame_distance < tracking_frame_distance:
                 frames_to_interpolate = frame_distance - 1
                 # interpolate back
                 for offset, dt in enumerate(np.linspace(0, 1, frames_to_interpolate + 2)[1:-1]):
@@ -187,22 +252,31 @@ def _interpolate_faces(frames, maximum_frame_depth=30, tracking_confidence=0.05)
     return frames
 
 
-def identify_faces_from_video(container: InputContainer, image_size=IDENTIFY_IMAGE_SIZE, progress=tqdm.tqdm):
+def identify_faces_from_video(container: InputContainer,
+                              detection_confidence=0.5,
+                              merge_confidence=0.25,
+                              tracking_frame_distance=30,
+                              tracking_confidence=0.05,
+                              image_size=IDENTIFY_IMAGE_SIZE,
+                              progress=tqdm.tqdm):
 
     faces = {stream.index: [] for stream in container.streams if stream.type == "video"}
 
-    with progress(desc="Detecting faces", total=container.video.frames, unit=" frames", leave=False) as progress:
-        # TODO Use both models: 0 for selfies, 1 for moderate distance
-        with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
-            for packet in container.demux():
-                if packet.stream.type == "video":
-                    for frame in packet.decode():
-                        image = frame.to_image()
-                        detected_faces = _identify_faces_from_image(image, face_detection, image_size)
-                        faces[packet.stream.index].append(detected_faces)
+    with FaceDetection(model_selection=0, min_detection_confidence=detection_confidence) as detection_close:
+        with FaceDetection(model_selection=1, min_detection_confidence=detection_confidence) as detection_far:
+            with progress(desc="Detecting faces", total=container.video.frames, unit=" frames", leave=False) as progress:
+                # TODO Use both models: 0 for selfies, 1 for moderate distance
+                for packet in container.demux():
+                    if packet.stream.type == "video":
+                        for frame in packet.decode():
+                            image = frame.to_image()
+                            detected_faces = _identify_faces_from_image(
+                                image, detection_close, detection_far, merge_confidence, image_size)
 
-                        if packet.stream == container.video:
-                            progress.update()
+                            faces[packet.stream.index].append(detected_faces)
+
+                            if packet.stream == container.video:
+                                progress.update()
 
     # Convert the coords to something meaningful
-    return {index: _interpolate_faces(faces) for index, faces in faces.items()}
+    return {index: _interpolate_faces(faces, tracking_frame_distance, tracking_confidence) for index, faces in faces.items()}
