dcbfbf4e17fa785ffef9efd95770fa8c84c912a7
Simona Dimitrova <nimeria1308@gmail.com>
2025-01-23 22:38:47 +0200

    Added reencode AV test

--- tests/test_av.py
+++ tests/test_av.py
@@ -1,7 +1,9 @@
 # Copyright (C) 2025, Simona Dimitrova
 
 import av.error
+import os
 import pytest
+import tempfile
 
 from faceblur.av.container import InputContainer, OutputContainer
 from faceblur.av.video import THREAD_TYPE_DEFAULT
@@ -28,3 +30,25 @@ def test_video_decode(filename):
                 except av.error.InvalidDataError as e:
                     # Drop the packet
                     pass
+
+
+@pytest.mark.parametrize("filename", VIDEO_FILES)
+def test_video_recode(filename):
+    with tempfile.TemporaryDirectory() as tempdir:
+        with InputContainer(filename, thread_type=THREAD_TYPE_DEFAULT) as input_container:
+            output = os.path.join(tempdir, os.path.basename(filename))
+            with OutputContainer(output, input_container) as output_container:
+                for packet in input_container.demux():
+                    assert packet
+
+                    if packet.stream.type == "video":
+                        try:
+                            for frame in packet.decode():
+                                assert frame
+
+                                # Encode + mux
+                                output_container.mux(frame)
+
+                        except av.error.InvalidDataError as e:
+                            # Drop the packet
+                            pass
