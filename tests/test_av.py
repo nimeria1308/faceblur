b1b1c6a0b18df06ee6e87257770b91fb848f9928
Simona Dimitrova <nimeria1308@gmail.com>
2025-01-23 22:31:24 +0200

    Use AUTO threading in av tests for faster operation and to avoid deadlocks in encoders

--- tests/test_av.py
+++ tests/test_av.py
@@ -4,19 +4,20 @@ import av.error
 import pytest
 
 from faceblur.av.container import InputContainer, OutputContainer
+from faceblur.av.video import THREAD_TYPE_DEFAULT
 from data import VIDEO_FILES
 
 
 @pytest.mark.parametrize("filename", VIDEO_FILES)
 def test_video_demux(filename):
-    with InputContainer(filename) as input_container:
+    with InputContainer(filename, thread_type=THREAD_TYPE_DEFAULT) as input_container:
         for packet in input_container.demux():
             assert packet
 
 
 @pytest.mark.parametrize("filename", VIDEO_FILES)
 def test_video_decode(filename):
-    with InputContainer(filename) as input_container:
+    with InputContainer(filename, thread_type=THREAD_TYPE_DEFAULT) as input_container:
         for packet in input_container.demux():
             assert packet
 
