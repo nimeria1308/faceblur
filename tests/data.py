c812b6c64ef7aabc7696da3bc429cbeef073e6a5
Simona Dimitrova <nimeria1308@gmail.com>
2025-01-23 22:54:53 +0200

    Move fate suite into subfolder

--- tests/data.py
+++ tests/data.py
@@ -1,5 +1,7 @@
 # Copyright (C) 2025, Simona Dimitrova
 
+import os
+
 from faceblur.av.container import EXTENSIONS as CONTAINER_EXENTSIONS
 from faceblur.image import EXTENSIONS as IMAGE_EXTENSIONS
 from faceblur.path import is_filename_from_ext_group
@@ -7,9 +9,11 @@ from faceblur.path import walk_files
 from pymediainfo import MediaInfo
 from subprocess import check_call
 
+TEST_DATA_FOLDER = "testdata"
+
 FFMPEG_FATE_SUITE_URL = "rsync://fate-suite.ffmpeg.org/fate-suite/"
 
-FFMPEG_FATE_SUITE = "testdata"
+FFMPEG_FATE_SUITE_FOLDER = os.path.join(TEST_DATA_FOLDER, "fate")
 
 FFMPEG_FATE_SKIPPED = [
     # PermissionError on demux()
@@ -182,15 +186,25 @@ FFMPEG_FATE_SKIPPED = [
     "cvid/catfight-cvid-pal8-partial.mov",
 ]
 
-# Make sure it is rsynced
-# Check fate-rsync target in FFmpeg/tests/Makefile
-check_call([
-    "rsync",
-    "-vrltLW", "--timeout=60",
-    FFMPEG_FATE_SUITE_URL, FFMPEG_FATE_SUITE
-])
 
-FFMPEG_FATE_FILES = walk_files(FFMPEG_FATE_SUITE, FFMPEG_FATE_SKIPPED)
+def _prepare_ffmpeg_fate_suite():
+    # Make sure FFMPEG fate suite it is rsynced
+    # Check fate-rsync target in FFmpeg/tests/Makefile
+    check_call([
+        "rsync",
+        "-vrltLW", "--timeout=60",
+        FFMPEG_FATE_SUITE_URL, FFMPEG_FATE_SUITE_FOLDER
+    ])
+
+
+def _prepare_files():
+    os.makedirs(TEST_DATA_FOLDER, exist_ok=True)
+    _prepare_ffmpeg_fate_suite()
+
+
+_prepare_files()
+
+FFMPEG_FATE_FILES = walk_files(FFMPEG_FATE_SUITE_FOLDER, FFMPEG_FATE_SKIPPED)
 
 # Select only relevant files
 IMAGE_FILES = [f for f in FFMPEG_FATE_FILES if is_filename_from_ext_group(f, IMAGE_EXTENSIONS)]
