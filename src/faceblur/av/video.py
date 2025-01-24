31b2dbfcf32500e040aaf881f10f1499e064ba51
Simona Dimitrova <nimeria1308@gmail.com>
2025-01-25 00:08:16 +0200

    2-pass encoding fails on some videos

--- src/faceblur/av/video.py
+++ src/faceblur/av/video.py
@@ -12,7 +12,6 @@ from faceblur.av.filter import Filter, Graph
 from faceblur.av.frame import Frame
 from faceblur.av.packet import Packet
 from PIL.Image import Image
-from av.codec.context import Flags
 
 THREAD_TYPES = [
     "SLICE",
@@ -217,10 +216,6 @@ class OutputVideoStream(OutputStream):
         output_stream.codec_context.height = input_stream.height
 
         # FIXME: bit_rate_tolerance is None even after setting it explicitly
-        # Instead of increasing the bit rate to accommodate low-rate encoding
-        # artifacts at beginning of stream for VFR videos, simply request
-        # a two-pass encoding
-        output_stream.codec_context.flags |= Flags.pass1 | Flags.pass2
 
         # Some codecs like mjpeg do not work well with multuthreading
         if encoder in CODECS_NEED_SINGLE_THREAD:
