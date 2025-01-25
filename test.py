84c1ff51128b66e21d7e1040203c39e5ddf5ff36
Simona Dimitrova <nimeria1308@gmail.com>
2025-01-25 18:36:06 +0200

    Enable extra logging for tests

--- test.py
+++ test.py
@@ -2,11 +2,17 @@
 
 # Copyright (C) 2025, Simona Dimitrova
 
+import av
 import logging
 import pytest
 import sys
 
 sys.path.append("src")
+
+# WARNING/libav.swscaler           (66753 ): deprecated pixel format used, make sure you did set range correctly
+logging_format = "%(levelname)-7s/%(name)-24s (%(process)-6d): %(message)s"
+logging.basicConfig(format=logging_format, level=logging.DEBUG)
+av.logging.set_level(av.logging.VERBOSE)
 
 if __name__ == "__main__":
     sys.exit(pytest.main(["tests"] + sys.argv[1:]))
