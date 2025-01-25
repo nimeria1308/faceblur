dfdde91bb3c604a0bb5b04e78005fc4afeb0505f
Simona Dimitrova <nimeria1308@gmail.com>
2025-01-25 19:40:09 +0200

    Fix: Some recode tests get stuck

--- test.py
+++ test.py
@@ -3,16 +3,14 @@
 # Copyright (C) 2025, Simona Dimitrova
 
 import av
-import logging
 import pytest
 import sys
 
 sys.path.append("src")
 
-# WARNING/libav.swscaler           (66753 ): deprecated pixel format used, make sure you did set range correctly
-logging_format = "%(levelname)-7s/%(name)-24s (%(process)-6d): %(message)s"
-logging.basicConfig(format=logging_format, level=logging.DEBUG)
-av.logging.set_level(av.logging.VERBOSE)
-
 if __name__ == "__main__":
+    if "--log-level=DEBUG" in sys.argv[1:]:
+        # Increase verbosity for av only if asked, as some encoders get stuck
+        av.logging.set_level(av.logging.VERBOSE)
+
     sys.exit(pytest.main(["tests"] + sys.argv[1:]))
